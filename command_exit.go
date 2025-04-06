d14d212ed937a6545cba8a60c53773675603fe39
FT1006 <frankthp1234@gmail.com>
2025-04-07 00:49:07 +0100

    support the up arrow to cycle through previous commands

--- command_exit.go
+++ command_exit.go
@@ -9,7 +9,14 @@ func commandExit(additionalInput string, c *Config) error {
 	if additionalInput != "" {
 		fmt.Println("Additional input has been ignored")
 	}
+
+	// Make sure we have a clean line with proper cursor position
+	// Clear any terminal escape sequences that might be in effect
+	os.Stdout.WriteString("\r\n")
+
+	// Print farewell message
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
+	// Use a sentinel error to signal exit
 	return nil
 }
